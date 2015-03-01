9c1e3f9cf8d00a3f6567ef59c5d3910d9f85ed13
wallaceg09 <wallaceg09@yahoo.com>
2015-03-01 08:02:29 -0600

    Implement NewTest.update Implement NewTest.move_back Implement moving sprite down due to gravity

--- game/NewTest.py
+++ game/NewTest.py
@@ -50,11 +50,14 @@ class Player(DirtySprite):
             self.image = sprite_sheet.image_at(source_rect, colorkey)#FIXME: Issue #2: Sprite outlined in color_key color (with green color_key value)
         else:
             self.image = sprite_sheet.image_at(source_rect).convert_alpha()
-            
+
         self.source_rect = source_rect
+        self.velocity = [0, 0]
         self._position = [0, 0]
         self._old_position = [0, 0]
-        self.rect = pygame.Rect(self.position[0], self.position[1], self.source_rect.width, self.source_rect.height)
+        self.rect = pygame.Rect(self._position[0], self._position[1], self.source_rect.width, self.source_rect.height)
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, self.rect.height * 0.2)
+        self.feet.midbottom = self.rect.midbottom
         
     @property
     def position(self):
@@ -65,7 +68,18 @@ class Player(DirtySprite):
         self._position = list(value)
         
     def update(self, delta):
+        self.velocity[1] += 10
+        self._old_position = self._position[:]
+        self._position[0] += self.velocity[0] * delta
+        self._position[1] += self.velocity[1] * delta
+        self.rect.topleft = self._position
+        self.feet.midbottom = self.rect.midbottom
         pass
+    
+    def mpve_back(self, delta):
+        self._position = self._old_position[:]
+        self.rect.topleft = self._position
+        self.feet.midbottom = self.rect.midbottom
     
 class ScrollGame(object):
     
@@ -75,19 +89,28 @@ class ScrollGame(object):
         pygame.init()
         self.screen_size = (400, 400)
         self.screen = pygame.display.set_mode(self.screen_size)
+        
         #Load map        #TODO: make this dynamic (i.e. process can load any map)
         self.tmx_data = util_pygame.load_pygame(get_map(MAP_FILENAME))
         self.map_data = pyscroll.TiledMapData(self.tmx_data)
+        
         #Initialize renderer
         self.map_layer = pyscroll.BufferedRenderer(self.map_data, self.screen_size, clamp_camera = True)
+        
         #Setup rendering group
         self.group = pyscroll.PyscrollGroup(map_layer = self.map_layer,
                                             default_layer = 0)
         print(self.tmx_data.get_layer_by_name("Player"))
+        
         #Add Player
         player_rect = pygame.Rect(0, 400, 100, 100)
         self.player = Player(PLAYER_FILENAME, player_rect, (0, 255, 0))  
         self.group.add(self.player)
+        
+        #Initialize collision map
+        self.collision = list()
+        for object in self.tmx_data.get_layer_by_name("Collision"):
+            pass
         pass
         
     def run(self):
