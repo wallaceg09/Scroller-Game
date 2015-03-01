f8f90d2731980c7d4f4a6915b2814eff7998053e
wallaceg09 <wallaceg09@yahoo.com>
2015-03-01 10:31:37 -0600

    Refactoring Gravity system Refactoring Movement system

--- game/NewTest.py
+++ game/NewTest.py
@@ -10,16 +10,16 @@ import os.path
 from pytmx import util_pygame
 from pygame.sprite import DirtySprite
 from SpriteSheet import spritesheet
+from pygame.constants import *
 
 RESOURCES_DIR = 'res'
-FPS = 60
-HERO_MOVE_SPEED = 200
 FPS = 60
 
 MAP_FILENAME = 'PlatformerDebugMap.tmx'
 PLAYER_FILENAME = 'wizard_green.png'
 
 SPRITESHEET_MAP = {}
+
 
 def get_map(filename):
     
@@ -36,7 +36,13 @@ def get_full_img_path(filename):
 #FIXME: Issue #1: Player spritesheet isn't maintaining transparency when being drawn
 #Workaround: use color_key. 
 class Player(DirtySprite):
+    MOVE_SPEED = 400
+    FALL_SPEED = 1000
+    DELTA_V = 50
+    GRAVITY = 50
     
+    #State enum hack
+    IDLE, MOVING_LEFT, MOVING_RIGHT, JUMPING, FALLING = range(5)
     def __init__(self, image_filename, source_rect, colorkey = None):
         DirtySprite.__init__(self)
         image_filename = get_full_img_path(image_filename)
@@ -58,6 +64,8 @@ class Player(DirtySprite):
         self.rect = pygame.Rect(self._position[0], self._position[1], self.source_rect.width, self.source_rect.height)
         self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, self.rect.height * 0.2)
         self.feet.midbottom = self.rect.midbottom
+        self.state = Player.IDLE
+        self.gravity = True
         
     @property
     def position(self):
@@ -68,7 +76,8 @@ class Player(DirtySprite):
         self._position = list(value)
         
     def update(self, delta):
-        self.velocity[1] += 10
+        self.update_velocity()
+        self.update_gravity()
         self._old_position = self._position[:]
         self._position[0] += self.velocity[0] * delta
         self._position[1] += self.velocity[1] * delta
@@ -76,7 +85,37 @@ class Player(DirtySprite):
         self.feet.midbottom = self.rect.midbottom
         pass
     
+    def update_gravity(self):
+        if self.gravity:
+            self.velocity[1] += Player.GRAVITY
+            self.velocity[1] = min(self.velocity[1] + Player.GRAVITY, Player.FALL_SPEED)
+        else:
+            self.velocity[1] = 0
+        pass
+    
+    def update_velocity(self):
+        if self.state == Player.IDLE:
+            if self.velocity[0] < 0:
+                self.velocity[0] = min(self.velocity[0] + Player.MOVE_SPEED, 0)
+            elif self.velocity[0] > 0:
+                self.velocity[0] = max(self.velocity[0] - Player.MOVE_SPEED, 0)
+        elif self.state == Player.MOVING_LEFT:
+            self.velocity[0] = max(self.velocity[0] - Player.DELTA_V, -Player.MOVE_SPEED)
+        elif self.state == Player.MOVING_RIGHT:
+            self.velocity[0] = min(self.velocity[0] + Player.DELTA_V, Player.MOVE_SPEED)
+            
+    def move_left(self):
+        self.state = Player.MOVING_LEFT
+    
+    def move_right(self):
+        self.state = Player.MOVING_RIGHT
+        
+    def set_idle(self):
+        self.state = Player.IDLE
+        
+    #TODO: Remove
     def move_back(self, delta):
+        self.velocity[1] = 0
         self._position = self._old_position[:]
         self.rect.topleft = self._position
         self.feet.midbottom = self.rect.midbottom
@@ -99,7 +138,7 @@ class ScrollGame(object):
         
         #Setup rendering group
         self.group = pyscroll.PyscrollGroup(map_layer = self.map_layer,
-                                            default_layer = 0)
+                                            default_layer = 4)
         print(self.tmx_data.get_layer_by_name("Player"))
         
         #Add Player
@@ -136,6 +175,16 @@ class ScrollGame(object):
         while event:
             if event.type == pygame.QUIT:
                 self.quit = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.quit = True
+                elif event.key == K_RIGHT:
+                    self.player.move_right()
+                elif event.key == K_LEFT:
+                    self.player.move_left()
+            elif event.type == pygame.KEYUP:
+                if event.key == K_RIGHT or event.key == K_LEFT:
+                    self.player.set_idle()
             event = pygame.event.poll()
         pass
     
@@ -145,7 +194,10 @@ class ScrollGame(object):
         #TODO: Check for collisions
         for sprite in self.group.sprites():
             if sprite.feet.collidelist(self.collisions) > -1:
-                sprite.move_back(delta)
+                sprite.gravity = False
+                #sprite.move_back(delta)
+            else:
+                sprite.gravity = True
         pass
     
     def _draw(self):
